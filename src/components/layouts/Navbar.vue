678a3969b7caf1536cf17bda067124fc72bdf497
nen02 <riverarenen02@gmail.com>
2020-09-21 08:23:02 +0800

    navbar dropdown

--- src/components/layouts/Navbar.vue
+++ src/components/layouts/Navbar.vue
@@ -5,22 +5,22 @@
     </a>
     <nav class="navbar-nav">
       <ul class="nav-items">
-        <li class="nav-item">
+        <li class="nav-item dropdown">
           <a href="#" class="nav-link">
             <fa icon="envelope"></fa>
             <span class="link-desc">Messages</span>
+            <span class="label label-danger">2</span>
           </a>
+          <ul class="dropdown-menu">
+            <li class="dropdown-header"><small>You have 2 unread message(s)</small></li>
+            <li class="dropdown-body">Body</li>
+            <li class="dropdown-footer text-center"><a href="#"><small>See All Messages</small></a></li>
+          </ul>
         </li>
         <li class="nav-item">
           <a href="#" class="nav-link">
             <fa icon="bell"></fa>
             <span class="link-desc">Notifications</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a href="#" class="nav-link">
-            <fa icon="user"></fa>
-            <span class="link-desc">User</span>
           </a>
         </li>
         <li class="nav-item user-acc">
